c175e19d11f7ae18a18c2060403853921be2c5b7
imam-hossain1 <zunayedemon6t9@gmail.com>
2024-02-26 21:58:12 +0600

    zod validation setup complete for user signup or login

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import { AuthController } from "./auth.controller";
+import validateRequest from "../../middlewares/validateRequest";
+import { AuthValidation } from "./auth.validation";
 
 const router = Router();
 
 router.post('/signup',
+validateRequest(AuthValidation.createUserZodSchema),
 AuthController.createUser);
 
 router.post('/login',
+validateRequest(AuthValidation.loginUserZodSchema),
 AuthController.logInUser);
 
 export const AuthRoutes = router;
---
+++ src/app/modules/auth/auth.validation.ts
@@ -0,0 +1,65 @@
+import { z } from 'zod';
+
+const createUserZodSchema = z.object({
+  body: z.object({
+    name: z.string({
+      required_error: 'name is required',
+    }),
+    email: z.string({
+      required_error: 'email is required',
+    }),
+    pin: z.string({
+      required_error: 'pin is required',
+    }),
+    number: z.number({
+      required_error: 'number is required',
+    }),
+    account_type: z.string({
+      required_error: 'account_type is required',
+    }),
+    nid: z.number({
+      required_error: 'nid is required',
+    }),
+  }),
+});
+
+const loginUserZodSchema = z.object({
+  body: z.object({
+    number: z.number({
+      required_error: 'number is required',
+    }),
+    pin: z.string({
+      required_error: 'pin is required',
+    })
+  }),
+});
+
+const refreshTokenZodSchema = z.object({
+  cookies: z.object({
+    refreshToken: z.string({
+      required_error: 'Refresh Token is required',
+    }),
+  }),
+});
+
+const changePinZodSchema = z.object({
+  body: z.object({
+    number: z.number({
+      required_error: 'number is required',
+    }),
+    oldPin: z.string({
+      required_error: 'oldPin is required',
+    }),
+    newPin: z.string({
+      required_error: 'newPin is required',
+    }),
+  }),
+});
+
+
+export const AuthValidation = {
+  createUserZodSchema,
+  loginUserZodSchema,
+  refreshTokenZodSchema,
+  changePinZodSchema
+};
